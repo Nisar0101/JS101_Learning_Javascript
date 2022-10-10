b39d3c58b4e0e1df09b500a83bb74c0d6bd1f40b
Nisar0101 <nisarshaikhm18@gmail.com>
2022-10-10 21:19:35 +0530

    variables_&_data_types

---
+++ introduction_to_js/problem2.js
@@ -0,0 +1,8 @@
+let name="Nisar";
+console.log(name);
+
+father="Manjur"
+console.log(father)
+
+mother="Jamila"
+console.log(mother)
---
+++ introduction_to_js/problem3.js
@@ -0,0 +1,4 @@
+let name="Nisar";
+age=22;
+console.log(name,age);
+console.log(typeof(name),typeof(age))
