8a1cbaa3e8e26ab5c0cbdc462590f1a71de7cfd9
Nisar0101 <nisarshaikhm18@gmail.com>
2022-10-12 18:43:33 +0530

    array_problems

---
+++ array/problem1.js
@@ -0,0 +1,7 @@
+//Problem 1 : Given an array print the position (starting with 1) and the element in a single line.
+
+let arr = ["chapter1", "chapter2", "chapter3", "chapter4", "chapter5"]
+
+for (let i = 0; i <= arr.length - 1; i++) {
+  console.log(i + 1, arr[i]);
+}
